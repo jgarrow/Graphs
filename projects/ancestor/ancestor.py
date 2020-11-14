30943033a477ee85e9350300a2f64badf25d0226
Janessa Garrow <janessa.garrow@gmail.com>
2020-11-14 11:47:22 -0700

    Finish earliest ancestor

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,4 +1,9 @@
 
+def get_parents(family_tree, child_node):
+    parents = [node[0] for node in family_tree if node[1] == child_node]
+
+    return parents
+
 def earliest_ancestor(ancestors, starting_node, path = [], visited = set()):
     # ancestors = array of tuples where tuple[0] is the parent and tuple[1] is the child it's connected to
 
@@ -8,15 +13,13 @@ def earliest_ancestor(ancestors, starting_node, path = [], visited = set()):
     # get "neighbors" -- parents on the fly instead of building entire graph up front
     # makes this more dynamic and efficient
 
-    def get_parents(family_tree, child_node):
-        parents = [node[0] for node in family_tree if node[1] == child_node]
-
-        return parents
-
     parents = get_parents(ancestors, starting_node)
 
-    print(f'starting_node: {starting_node}')
-    print(f'parents: {parents}')
+    # if this is the first function call
+    # path is empty
+    # "clean" our visited set to be an empty set in case of persisted data from previous tests
+    if len(path) == 0:
+        visited = set()
 
     # if the starting_node is the earliest ancestor
     if len(parents) == 0 and len(path) == 0:
@@ -28,7 +31,6 @@ def earliest_ancestor(ancestors, starting_node, path = [], visited = set()):
     path = path + [starting_node]
 
     if len(parents) == 0:
-        print(f'no more parents: {path}')
         return path[-1]
     
     for parent in parents:
@@ -36,9 +38,4 @@ def earliest_ancestor(ancestors, starting_node, path = [], visited = set()):
             new_path = earliest_ancestor(ancestors, parent, path, visited)
 
             if new_path is not None:
-                print(f'not none: {new_path}')
                 return new_path
-    
-    
-    
-
